ccba5dec54bf3f79a9dc4038ae83f48dc268433d
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2023-06-22 02:48:34 +0000

    feat: routes for admin and profile

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  namespace :profiles_backoffice do
+    get 'welcome/index'
+  end
+  namespace :admins_backoffice do
+    get 'welcome/index'
+  end
   devise_for :admins
   devise_for :profiles
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
