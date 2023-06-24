1f52bc6d7c798ff872d44674a200302378a06c44
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2023-06-24 13:26:48 +0000

    feat: created the admins_backoffice controller

---
+++ app/controllers/admins_backoffie_controller.rb
@@ -0,0 +1,2 @@
+class AdminsBackoffieController < ApplicationController
+end
---
+++ test/controllers/admins_backoffie_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class AdminsBackoffieControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
